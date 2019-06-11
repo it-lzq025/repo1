c3d5e991ed155e7b06be3ee4c33de95a9d10faaa
mx <824986211@qq.com>
2019-06-11 16:10:25 +0800

    第一次IDEA提交代码

---
+++ utils/Num.java
@@ -0,0 +1,21 @@
+package com.itheima.ssm.utils;
+
+
+//逢指定数字过
+public class Num {
+    public static void main(String[] args) {
+       /*
+       for (int i = 0; i <=300; i++) {
+           if((i+"").contains("9") || i%9==0){
+               System.out.println(i);
+           }
+        }
+        */
+
+        if("our".contains("or")){
+            System.out.println("猜想正确");
+        }else {
+            System.out.println("猜想错误");
+        }
+    }
+}
